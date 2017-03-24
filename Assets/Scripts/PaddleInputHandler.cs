577f3f906497be06cbb01164134a47d6003ca57a
Alicia G <firenz.shizu@gmail.com>
2017-03-24 17:30:17 +0100

    Added PaddleInputHandler.cs script

---
+++ Assets/Scripts/PaddleInputHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleInputHandler : MonoBehaviour {
+
+    #region private
+    [SerializeField]
+    private float speed = 8f;
+    [SerializeField]
+    private string axis = "Vertical";
+
+    private Rigidbody thisRigidbody;
+    #endregion
+
+    private void Start()
+    {
+        thisRigidbody = this.transform.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float currentVelocity = Input.GetAxisRaw(axis);
+        thisRigidbody.velocity = Vector3.up * speed * currentVelocity;
+    }
+}
